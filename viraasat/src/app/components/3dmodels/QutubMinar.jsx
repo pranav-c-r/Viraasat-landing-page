839785d07e77926ae9097de423440922ca50fc83
Pranav C R <pranavcr001@gmail.com>
2025-09-20 10:28:56 +0530

    added 3d models with zoom effect into landing page

---
+++ viraasat/src/app/components/3dmodels/QutubMinar.jsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+
+function Model() {
+  const { scene } = useGLTF('/models/qutubminar_base_basic_shaded.glb')
+  return <primitive object={scene} scale={1} />
+}
+
+export default function QutubMinar() {
+  return (
+    <div className="w-full h-screen">
+      <Canvas camera={{ position: [0, 1, 3], fov: 50 }}>
+        {/* Lights */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} />
+
+        {/* Model */}
+        <Model />
+
+        {/* Controls */}
+        <OrbitControls enableZoom={true} />
+      </Canvas>
+    </div>
+  )
+}
