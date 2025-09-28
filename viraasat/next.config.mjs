064c335558c22fe847bfb6fc37e067ec73300881
Noel Manoj <noelmanoj24bcs46@iiitkottayam.ac.in>
2025-09-28 11:32:07 +0530

    Enhance heritage site details and add image configuration for optimized loading

--- viraasat/next.config.mjs
+++ viraasat/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.pexels.com', 'images.unsplash.com', 'picsum.photos'],
+  },
+};
 
 export default nextConfig;
